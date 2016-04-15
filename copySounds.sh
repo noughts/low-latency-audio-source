5a71e4fb1a0ae83f0bc75a3ebc59dc5e602e79c0
Koichi Yamamoto <noughts@gmail.com>
2016-04-15 16:48:52 +0900

    a

--- copySounds.sh
+++ copySounds.sh
@@ -1,2 +1,7 @@
 mkdir -p Assets/StreamingAssets/Resources/Sounds/
 cp -r Assets/example/Resources/Sounds/ Assets/StreamingAssets/Resources/Sounds/
+
+cd Assets/StreamingAssets/Resources/Sounds/
+for file in `\find . -maxdepth 1 -type f`; do
+    echo $file
+done
