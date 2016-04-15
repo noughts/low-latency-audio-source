16f467e5ee9c3dbe96cf2c0a705e532f2ed0581c
Koichi Yamamoto <noughts@gmail.com>
2016-04-15 18:09:55 +0900

    a

--- androidProject/lowlatencyaudiosourceplugin/src/main/java/jp/dividual/lowLatencyAudioSourcePlugIn/LowLatencyAudioSourcePlugIn.java
+++ androidProject/lowlatencyaudiosourceplugin/src/main/java/jp/dividual/lowLatencyAudioSourcePlugIn/LowLatencyAudioSourcePlugIn.java
@@ -1,14 +1,33 @@
 package jp.dividual.lowLatencyAudioSourcePlugIn;
 
 import android.app.Activity;
+import android.content.res.AssetFileDescriptor;
+import android.media.MediaPlayer;
+import android.util.Log;
 import android.widget.Toast;
 import com.unity3d.player.UnityPlayer;
 
-public class LowLatencyAudioSourcePlugIn {
+import java.io.IOException;
+
+public class LowLatencyAudioSourcePlugin {
 
 	private Activity _activity;
+	private MediaPlayer _mediaPlayer;
 
-	public LowLatencyAudioSourcePlugIn( Activity activity ){
+	public LowLatencyAudioSourcePlugin(Activity activity ){
 		_activity = activity;
+		_mediaPlayer = new MediaPlayer();
+	}
+
+
+	public void loadSound( String soundName ){
+		AssetFileDescriptor afd;
+
+		try {
+			afd = _activity.getAssets().openFd( soundName );
+		} catch( IOException e ) {
+			Log.e( "SoundPluginUnity", "File does not exist!" );
+			return;
+		}
 	}
 }
