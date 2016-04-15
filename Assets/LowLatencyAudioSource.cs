62a6e96024ea441d8cba97e1ba489726aaa8c1b1
Koichi Yamamoto <noughts@gmail.com>
2016-04-15 15:51:11 +0900

    a

---
+++ Assets/LowLatencyAudioSource.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LowLatencyAudioSource : MonoBehaviour {
+
+	/// iOS、エディタ用のaudioSource;
+	AudioSource audioSource;
+
+	void Awake(){
+		audioSource = gameObject.AddComponent<AudioSource> ();
+	}
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+
+	public void play(AudioClip clip){
+		audioSource.PlayOneShot (clip);
+	}
+
+
+}
---
+++ Assets/MainController.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MainController : MonoBehaviour {
+
+	public LowLatencyAudioSource audioSource;
+	public AudioClip clip;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+
+	public void playSound(){
+		audioSource.play (clip);
+	}
+
+
+}
