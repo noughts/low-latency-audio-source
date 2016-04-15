a67f121f616b449d1b378c2c7d24f966f3f1b986
Koichi Yamamoto <noughts@gmail.com>
2016-04-15 18:29:09 +0900

    a

---
+++ buildPlugin.sh
@@ -0,0 +1,5 @@
+# androidProject/gradlew jar
+
+from="/Users/noughts/Dropbox/works/libs/unity/LowLatencyAudioSource/androidProject/lowlatencyaudiosourceplugin/build/libs/LowLatencyAudioSourcePlugIn-1.0.0.jar"
+to="/Users/noughts/Dropbox/works/libs/unity/LowLatencyAudioSource/Assets/Plugins/Android/"
+cp ${from} ${to}
